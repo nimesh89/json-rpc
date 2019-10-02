b6062e256611649816dcfc3c7354ebccf3ae74c7
nimesh.pramuditha <nimesh.pramuditha@ALSG-G31A132-D>
2019-10-02 17:04:01 +0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using StreamJsonRpc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.WebSockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                var formatter = new JsonMessageFormatter(Encoding.UTF8);
+                using (ClientWebSocket ws = new ClientWebSocket())
+                using (var wsh = new WebSocketMessageHandler(ws, formatter))
+                using (JsonRpc rpc = new JsonRpc(wsh))
+                {
+                    Uri serverUri = new Uri("wss://app.sensemetrics.com:4201");
+                    ws.ConnectAsync(serverUri, CancellationToken.None).Wait();
+
+                    rpc.Disconnected += Rpc_Disconnected;
+
+                    rpc.StartListening();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.WriteLine(ex.StackTrace);
+            }
+        }
+
+        private static void Rpc_Disconnected(object sender, JsonRpcDisconnectedEventArgs e)
+        {
+            Console.WriteLine(e.Reason);
+            Console.WriteLine(e.LastMessage);
+            Console.WriteLine(e.Description);
+            if(e.Exception != null)
+            {
+                Console.WriteLine(e.Exception.Message);
+                Console.WriteLine(e.Exception.StackTrace);
+            }
+        }
+    }
+}
